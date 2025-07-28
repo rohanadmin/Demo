7142280d038fc330a424ff0887d7d983eaac5366
rohanadmin <imrohans45@gmail.com>
2025-07-28 16:24:31 +0530

    done by architech

--- Anagram.java
+++ Anagram.java
@@ -17,6 +17,21 @@ public class Anagram {
         return Arrays.equals(arr1,arr2);
     }
 
+    public static Boolean checkAnagram1(String str, String str1){
+
+        if(str.length()!=str1.length()){
+            return false;
+        }
+
+        char[] arr1 = str.toLowerCase().toCharArray();
+        char[] arr2 = str1.toLowerCase().toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        return Arrays.equals(arr1,arr2);
+    }
+
     public static void main(String[] args){
 
         String s1= "listen";
