bcbc04c1d4463e990bf613c9b37041085bbf7739
rohanadmin <imrohans45@gmail.com>
2025-07-28 16:31:15 +0530

    main Architect

--- Anagram.java
+++ Anagram.java
@@ -31,6 +31,21 @@ public class Anagram {
 
         return Arrays.equals(arr1,arr2);
     }
+	
+	public static Boolean checkAnagram2(String str, String str1){
+
+        if(str.length()!=str1.length()){
+            return false;
+        }
+
+        char[] arr1 = str.toLowerCase().toCharArray();
+        char[] arr2 = str1.toLowerCase().toCharArray();
+
+        Arrays.sort(arr1);
+        Arrays.sort(arr2);
+
+        return Arrays.equals(arr1,arr2);
+    }
 
     public static void main(String[] args){
 
