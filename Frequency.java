ffe323461a183004b5082ba3b42f667c713e869a
rohanadmin <imrohans45@gmail.com>
2025-07-28 16:50:37 +0530

    New file added

---
+++ Frequency.java
@@ -0,0 +1,19 @@
+public class Frequency {
+
+    public static void main(String[] args){
+
+        String str="aabbcdcbd";
+
+
+        for(int i=0;i<str.length();i++){
+            int count = 1;
+            char ch = str.charAt(i);
+
+            for(int j=i+1;j<str.length();j++){
+                if(ch == str.charAt(j)){
+
+                }
+            }
+        }
+    }
+}
